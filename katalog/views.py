93c34351d557341aec0bf3c4f1e8407f7d8540cd
hapsarimeilany <hapsarimeilany@gmail.com>
2022-09-14 15:58:54 +0700

    Commit Katalog Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+from katalog.models import CatalogItem
+...
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Meilany',
+    'NPM' : '2106751436'
+}
+    return render(request, "katalog.html", context)
